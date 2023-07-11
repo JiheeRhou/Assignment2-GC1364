914745cce7b61c1e64f2b1fcbde89fac42177af9
JiheeRhou <200521364@student.georgianc.on.ca>
2023-07-11 10:22:17 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.georgiancollege.assignment2gc1364 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.georgiancollege.assignment2gc1364 to javafx.fxml;
+    exports com.georgiancollege.assignment2gc1364;
+}

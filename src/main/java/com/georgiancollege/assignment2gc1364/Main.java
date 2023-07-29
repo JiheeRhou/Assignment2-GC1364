59e04574ecf3592741eb3d83e48dcaec7f742910
JiheeRhou <200521364@student.georgianc.on.ca>
2023-07-29 14:27:30 -0400

    Change the icon

--- src/main/java/com/georgiancollege/assignment2gc1364/Main.java
+++ src/main/java/com/georgiancollege/assignment2gc1364/Main.java
@@ -3,6 +3,7 @@ package com.georgiancollege.assignment2gc1364;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,6 +18,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View/search-recipe-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image("file:src/main/resources/com/georgiancollege/assignment2gc1364/Images/recipe.png"));
         stage.setTitle("Search Recipes!");
         stage.setScene(scene);
         stage.show();

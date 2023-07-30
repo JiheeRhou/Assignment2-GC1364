a19308745e64158d5639f7530a90ae43f4045f4c
JiheeRhou <200521364@student.georgianc.on.ca>
2023-07-29 20:46:55 -0400

    Add Error Label CSS styling

--- src/main/java/com/georgiancollege/assignment2gc1364/Controllers/SearchRecipeController.java
+++ src/main/java/com/georgiancollege/assignment2gc1364/Controllers/SearchRecipeController.java
@@ -40,6 +40,9 @@ public class SearchRecipeController implements Initializable {
     @FXML
     private Button getDetailsButton;
 
+    @FXML
+    private Label errorLabel;
+
     /**
      * This is a method to initialize the scene
      * get the data from the api and display the scene
@@ -48,17 +51,33 @@ public class SearchRecipeController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // getDetailsButton set not visible
         getDetailsButton.setVisible(false);
+        // titleLabel set not visible
         titleLabel.setVisible(false);
+        // errorLabel set not visible
+        errorLabel.setVisible(false);
+        // cuisineImageView set not visible
+        cuisineImageView.setVisible(false);
+        // set cuisineComboBox items
         cuisineComboBox.getItems().addAll("All", "African", "Asian", "American", "British", "Cajun", "Caribbean", "Chinese", "Eastern European", "European", "French", "German", "Greek", "Indian", "Irish", "Italian", "Japanese", "Jewish", "Korean", "Latin American", "Mediterranean", "Mexican", "Middle Eastern", "Nordic", "Southern", "Spanish", "Thai", "Vietnamese");
         cuisineComboBox.setValue("All");
+        // add Listener
         cuisineListView.getSelectionModel().selectedItemProperty().addListener(((observableValue, oldCuisine, newCuisine) -> {
+            // cuisineImageView set visible
+            cuisineImageView.setVisible(true);
+
             try {
+                // getDetailsButton set visible
                 getDetailsButton.setVisible(true);
+                // titleLabel set not visible
                 titleLabel.setVisible(true);
+                // titleLabel set title
                 titleLabel.setText(newCuisine.getTitle());
+                // cuisineImageView set image
                 cuisineImageView.setImage(new Image(newCuisine.getImage()));
             } catch (Exception e) {
+                // cuisineImageView set no-image
                 cuisineImageView.setImage(new Image("https://static.wikia.nocookie.net/undead-unluck/images/2/22/No_Image_available.png/revision/latest?cb=20200119200005"));
             }
         }));
@@ -73,6 +92,14 @@ public class SearchRecipeController implements Initializable {
     void searchButton_onClick(ActionEvent event) {
         // clear the cuisineListView
         cuisineListView.getItems().clear();
+        // getDetailsButton set not visible
+        getDetailsButton.setVisible(false);
+        // titleLabel set not visible
+        titleLabel.setVisible(false);
+        // errorLabel set not visible
+        errorLabel.setVisible(false);
+        // cuisineImageView set not visible
+        cuisineImageView.setVisible(false);
         // get user's selected cuisine from the cuisineComboBox
         String cuisine = cuisineComboBox.getSelectionModel().getSelectedItem();
         // get user's search text from the searchTextField
@@ -81,10 +108,13 @@ public class SearchRecipeController implements Initializable {
 //        ApiResponse apiResponse = ApiUtility.getDataFromFile("C:\\GeorgianCollege\\Java\\Assignment2-GC1364\\apiResponse.json");
         // get objects from Api response
         ApiResponse apiResponse = ApiUtility.getDataFromApi(cuisine, searchText);
-        if (apiResponse != null) {
+        if (apiResponse.getTotalResults() > 0) {
             // set the cuisineListView with the objects
             cuisineListView.getItems().addAll(apiResponse.getCuisines());
-
+            cuisineListView.getSelectionModel().selectFirst();
+        } else {
+            // errorLabel set visible
+            errorLabel.setVisible(true);
         }
 
     }
